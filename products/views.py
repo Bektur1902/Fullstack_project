b9bcaef2344e607f2264c94d29e62af0fb04b5fb
Bektur1902 <bektur1902@gmail.com>
2022-07-15 18:23:25 +0600

    Add Categoryserializer

--- products/views.py
+++ products/views.py
@@ -1,10 +1,36 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework import permissions
 
-from .serializers import ProductSerializer
-from .models import Product
+from .serializers import ProductSerializer, CategorySerializer
+from .models import Product, Category
 # Create your views here.
 
 
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return ProductSerializer
+        return super().get_serializer_class()
+
+    def get_permissions(self):
+        if self.action in ['list', 'retrieve']:
+            self.permission_classes = [permissions.AllowAny]
+        elif self.action in ['destroy', 'update', 'partial_update', 'create']:
+            self.permission_classes = [permissions.IsAdminUser]
+        return super().get_permissions()
+
+
+class CategoryViewSet(ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+    def get_permissions(self):
+        if self.action in ['list', 'retrieve']:
+            self.permission_classes = [permissions.AllowAny]
+        elif self.action in ['destroy', 'update', 'partial_update', 'create']:
+            self.permission_classes = [permissions.IsAdminUser]
+        return super().get_permissions()
+
